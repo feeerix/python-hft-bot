db0d4274e7c3a418305bba39986b078b0f2960f1
c01952f666540050efb7f2750d8e3e91 <felix.voo@ledger.com>
2023-03-19 21:51:20 +0100

    added a static lightweight-charts instance

---
+++ frontend/pages/testChart.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { createChart } from 'lightweight-charts';
+import { useEffect, useRef } from 'react';
+
+function TestChart() {
+    const chartContainerRef = useRef<HTMLDivElement>(null);
+    
+    useEffect(() => {
+        
+        const container = chartContainerRef.current;
+        if (container) {
+        const chart = createChart(container, { width: 1600, height: 800 });
+
+        const lineSeries = chart.addLineSeries();
+        lineSeries.setData([
+            { time: '2019-04-11', value: 80.01 },
+            { time: '2019-04-12', value: 96.63 },
+            { time: '2019-04-13', value: 76.64 },
+            { time: '2019-04-14', value: 81.89 },
+            { time: '2019-04-15', value: 74.43 },
+            { time: '2019-04-16', value: 80.01 },
+            { time: '2019-04-17', value: 96.63 },
+            { time: '2019-04-18', value: 16.64 },
+            { time: '2019-04-19', value: 91.89 },
+            { time: '2019-04-20', value: 71.43 },
+        ]);
+
+        return () => {
+            if (chart) {
+              chart.remove();
+            }
+        };
+      }
+    }, []);
+  
+    return <div ref={chartContainerRef} />
+  }
+
+export default React.memo(TestChart);
